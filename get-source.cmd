d48a56b2dcdd4f3544512ce28d64400386df7aee
Frederik Carlier <frederik.carlier@quamotion.mobi>
2019-01-09 10:54:34 +0100

    Add Azure Pipelines script

---
+++ get-source.cmd
@@ -0,0 +1,5 @@
+git clone https://github.com/libimobiledevice-win32/libplist
+git clone https://github.com/libimobiledevice-win32/libusbmuxd
+git clone https://github.com/libimobiledevice-win32/libimobiledevice
+git clone https://github.com/libimobiledevice-win32/libirecovery
+git clone https://github.com/libimobiledevice-win32/idevicerestore
