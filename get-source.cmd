b7bead749a0ebf1602b3f8ac7b9e5b9985e3d606
Frederik Carlier <frederik.carlier@quamotion.mobi>
2019-06-13 16:22:34 +0200

    Target Windows 10, include ideviceactivation

--- get-source.cmd
+++ get-source.cmd
@@ -1,6 +1,7 @@
 git clone --single-branch --branch msvc/master https://github.com/libimobiledevice-win32/libplist
 git clone --single-branch --branch msvc/master https://github.com/libimobiledevice-win32/libusbmuxd
 git clone --single-branch --branch msvc/master https://github.com/libimobiledevice-win32/libimobiledevice
+git clone --single-branch --branch msvc/master https://github.com/libimobiledevice-win32/libideviceactivation
 git clone https://github.com/libimobiledevice-win32/libirecovery
 git clone https://github.com/libimobiledevice-win32/idevicerestore
 git clone https://github.com/libimobiledevice-win32/usbmuxd
