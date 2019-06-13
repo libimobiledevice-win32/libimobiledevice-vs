d1cc64ffb24c61f336c3ad9322d7878393fd108f
Frederik Carlier <frederik.carlier@quamotion.mobi>
2019-06-13 22:45:40 +0200

    Add libirecovery

--- get-source.cmd
+++ get-source.cmd
@@ -3,6 +3,6 @@ git clone --single-branch --branch msvc/master https://github.com/libimobiledevi
 git clone --single-branch --branch msvc/master https://github.com/libimobiledevice-win32/libimobiledevice
 git clone --single-branch --branch msvc/master https://github.com/libimobiledevice-win32/libideviceactivation
 git clone --single-branch --branch msvc/master https://github.com/libimobiledevice-win32/ideviceinstaller
-git clone https://github.com/libimobiledevice-win32/libirecovery
+git clone --single-branch --branch msvc/master https://github.com/libimobiledevice-win32/libirecovery
 git clone https://github.com/libimobiledevice-win32/idevicerestore
 git clone https://github.com/libimobiledevice-win32/usbmuxd
