21b5763e9ab329a1bdd259ce72e93cffe13949a1
Frederik Carlier <frederik.carlier@quamotion.mobi>
2019-06-13 23:25:03 +0200

    Build idevicerestore

--- get-source.sh
+++ get-source.sh
@@ -4,5 +4,5 @@ git clone --single-branch --branch msvc/master https://github.com/libimobiledevi
 git clone --single-branch --branch msvc/master https://github.com/libimobiledevice-win32/libideviceactivation
 git clone --single-branch --branch msvc/master https://github.com/libimobiledevice-win32/ideviceinstaller
 git clone --single-branch --branch msvc/master https://github.com/libimobiledevice-win32/libirecovery
-git clone https://github.com/libimobiledevice-win32/idevicerestore
+git clone --single-branch --branch msvc/master https://github.com/libimobiledevice-win32/idevicerestore
 git clone https://github.com/libimobiledevice-win32/usbmuxd
