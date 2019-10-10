2b383cc89fd9f1c78db327f76931a4a54563e96c
Frederik Carlier <frederik.carlier@quamotion.mobi>
2019-10-10 12:09:40 +0200

    Print source revision information

--- get-source.sh
+++ get-source.sh
@@ -7,3 +7,30 @@ git clone --single-branch --branch msvc/master https://github.com/libimobiledevi
 git clone --single-branch --branch msvc/master https://github.com/libimobiledevice-win32/idevicerestore
 git clone --single-branch --branch master-msvc https://github.com/libimobiledevice-win32/usbmuxd
 git clone --single-branch --branch msvc/master https://github.com/libimobiledevice-win32/ios-webkit-debug-proxy
+
+echo "libplist revision:"
+git -C libplist rev-parse HEAD
+
+echo "libusbmxud revision:"
+git -C libusbmuxd rev-parse HEAD
+
+echo "libimobiledevice revision:"
+git -C libimobiledevice rev-parse HEAD
+
+echo "libideviceactivation revision:"
+git -C libideviceactivation rev-parse HEAD
+
+echo "ideviceinstaller revision:"
+git -C ideviceinstaller rev-parse HEAD
+
+echo "libirecovery revision:"
+git -C libirecovery rev-parse HEAD
+
+echo "idevicerestore revision:"
+git -C idevicerestore rev-parse HEAD
+
+echo "usbmuxd revision:"
+git -C usbmuxd rev-parse HEAD
+
+echo "ios-webkit-debug-proxy revision:"
+git -C ios-webkit-debug-proxy rev-parse HEAD
